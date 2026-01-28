f5bf691d4700179674526491409128a3844bac98
aryan9855 <aryansinghalagra@gmail.com>
2026-01-29 02:18:55 +0530

    day 16

--- Server/index.js
+++ Server/index.js
@@ -25,10 +25,15 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL || "http://localhost:3000",
-        credentials:true
+      origin: "http://localhost:5173",
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
     })
-)
+  );
+  
+
+  
 
 app.use(
     fileUpload({
